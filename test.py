d8e0f351da78f2ab212d2e29b07b0a8e23dc3424
denisse <dlgon695@gmail.com>
2025-08-06 17:51:27 -0600

    feat: new file test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+"""
+Unit tests for trivia_fetch function in trivia.py module.
+1.- Instalar pytest: pip install pytest
+2.- Ejecutar los tests: pytest test.py
+3.- Ver resultados: pytest test.py -v
+"""
+from trivia import trivia_fetch
+
+# Test 1
+def test_trivia_42():
+  assert trivia_fetch(42)["number"] == 42
+
+# Test 2
+def test_trivia_1000():
+  assert trivia_fetch(1000)["number"] == 1000
